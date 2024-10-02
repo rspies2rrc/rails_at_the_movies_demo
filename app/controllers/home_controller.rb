bcc2731f0a8b8c388246a1c8ee4f621886e6dfa6
rspies2rrc <rspies2@rrc.ca>
2024-10-02 15:30:31 -0500

    added top 10 movies to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @movies = Movie.includes(:production_company).order("avg_vote DESC").limit(10)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   resources :movies # , only: %i[:index, :show]
   resources :production_companies # , only: %i[:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
