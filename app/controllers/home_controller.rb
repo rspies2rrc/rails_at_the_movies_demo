dc9814c643754c7dced5b6bafb604df4e3215665
rspies2rrc <rspies2@rrc.ca>
2024-10-02 15:33:50 -0500

    added placeholder for top 10 production companies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
     @movies = Movie.includes(:production_company).order("avg_vote DESC").limit(10)
+
+    @production_companies = ProductionCompany.all.limit(10)
   end
 end
